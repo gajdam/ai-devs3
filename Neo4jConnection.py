a501b3d919e93e438fbdf309c7713cebddfc53c4
Maciek Gajda <gajdam11@gmail.com>
2025-01-09 00:39:01 +0100

    Graph database connection class added

---
+++ Neo4jConnection.py
@@ -0,0 +1,37 @@
+import os
+
+from neo4j import GraphDatabase
+from dotenv import load_dotenv
+
+
+class Neo4jConnection:
+    def __init__(self, uri, user, password):
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self._driver.close()
+
+    def query(self, query, parameters=None):
+        with self._driver.session() as session:
+            result = session.run(query, parameters)
+            records = [record.data() for record in result]
+            summary = result.consume()
+            return records, summary
+
+# load_dotenv()
+# uri = os.getenv("NEO4J_URI")
+# user = os.getenv("NEO4J_USER")
+# password = os.getenv("NEO4J_PASSWORD")
+#
+# conn = Neo4jConnection(uri, user, password)
+#
+# try:
+#     conn.query("CREATE (n:TestNode {name: 'Test'}) RETURN n")
+#
+#     result, _ = conn.query("MATCH (n:TestNode {name: 'Test'}) RETURN n.name AS name")
+#     print("Node created, name:", result)
+#
+# except Exception as e:
+#     print("Error occurred:", e)
+#
+# conn.close()
